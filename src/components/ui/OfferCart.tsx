4fad5e1e056f82ace110b636831a72222a3c832b
modecode22 <epiccoder00@gmail.com>
2023-07-01 17:33:22 +0100

    finished the request of ali

--- src/components/ui/OfferCart.tsx
+++ src/components/ui/OfferCart.tsx
@@ -2,7 +2,7 @@ import { getTimeDifference } from "@/lib/utils";
 import Image from "next/image";
 import { AiOutlineCheckCircle, AiOutlineClockCircle, AiOutlineExclamationCircle, AiOutlineHeart } from "react-icons/ai";
 import { IoLocationOutline } from "react-icons/io5";
-import { BsSend } from "react-icons/bs";
+import { FiHeart, FiSend } from "react-icons/fi";
 interface OfferCartProps {
     data:OfferCart
 }
@@ -18,8 +18,8 @@ const theDifference = getTimeDifference(data.createdAt);
 
 
   return (
-    <section className="w-full max-w-4xl md:aspect-[10/2.6] aspect-video  sm:aspect-[10/4]  bg-gray-50 drop-shadow-md flex flex-col">
-      <main className="flex justify-between  p-5 sm:flex-row w-full flex-col flex-grow">
+    <section className="w-full max-w-4xl md:aspect-[10/2.6] aspect-[16/9]  sm:aspect-[10/4]  bg-gray-50 drop-shadow-md flex flex-col">
+      <main className="flex justify-between  p-5 sm:flex-row w-full flex-col flex-grow gap-5">
         <section className="flex flex-col gap-3">
           <div className="flex  items-center gap-3">
             <section className=" relative w-14 h-14 rounded-full overflow-hidden  shadow-inner  ">
@@ -32,9 +32,9 @@ const theDifference = getTimeDifference(data.createdAt);
             </section>
             <section className="flex flex-col items-center justify-center">
               <h2 className="text-lg font-bold">{data.userName}</h2>
-              <div className="flex gap-2 items-center justify-center">
+              <div className="flex gap-2 items-center ">
                 <AiOutlineClockCircle className="fill-gray-400" />
-                <h3>{theDifference}</h3>
+                <h3 className="text-sm text-gray-500">{theDifference}</h3>
               </div>
             </section>
           </div>
@@ -110,16 +110,16 @@ const theDifference = getTimeDifference(data.createdAt);
           </div>
         </section>
       </main>
-      <hr />
-      <footer className="h-12  w-full flex justify-between items-center p-3">
+      <hr className="hidden sm:flex" />
+      <footer className="select-none sm:h-12 h-16  w-full flex justify-between items-center sm:p-3 p-5">
         <section className="flex justify-center items-center gap-5">
-          <div className="flex justify-center items-center gap-2">
+          <div className="group flex hover:text-red-600  active:text-red-500 cursor-pointer justify-center items-center gap-2">
             <h4> {data.likes.length} </h4>
-            <AiOutlineHeart />
+            <FiHeart className="group-active:fill-red-500 group-active:stroke-red-500" />
           </div>
-          <div className="flex justify-center items-center gap-2">
+          <div className="group hover:text-cyan-600  active:text-cyan-500 cursor-pointer flex justify-center items-center gap-2">
             <h4> {data.comments.length} </h4>
-            <BsSend />
+            <FiSend className="group-active:fill-cyan-500 group-active:stroke-cyan-500" />
           </div>
         </section>
         <button className="rounded-full bg-gray-900 text-gray-200 hover:text-gray-100 active:text-gray-50 active:bg-gray-800 p-2 px-14 flex justify-center items-center">
